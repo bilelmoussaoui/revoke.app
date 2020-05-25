b1a0e523029758ef2e98cc4e93013d608a7f51a2
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-05-26 01:23:55 +0200

    init version

---
+++ static/app.js
@@ -0,0 +1,22 @@
+document.addEventListener(
+  'DOMContentLoaded',
+  function () {
+    function search (pattern) {
+      const $tiles = document.querySelectorAll('div.tile')
+      $tiles.forEach($tile => {
+        console.log($tile.querySelector('.title'))
+        const title = $tile.querySelector('p.title').innerHTML
+        if (title.toLowerCase().includes(pattern.toLowerCase())) {
+          $tile.parentNode.style.display = 'block'
+        } else {
+          $tile.parentNode.style.display = 'none'
+        }
+      })
+    }
+
+    const $searchInput = document.querySelector('#searchInput')
+    $searchInput.addEventListener('keyup', () => search($searchInput.value))
+    $searchInput.addEventListener('change', () => search($searchInput.value))
+  },
+  false
+)
